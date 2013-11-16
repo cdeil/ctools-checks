f9d858db84600f1cd8838f63cf4b92a772a3e6c3
Christoph Deil <Deil.Christoph@gmail.com>
2013-11-16 21:22:23 +0100

    update

--- compile_power_law.sh
+++ compile_power_law.sh
@@ -3,7 +3,8 @@ $ pkg-config --cflags gammalib
 $ pkg-config --libs gammalib
 -L/usr/local/gamma/lib -lgamma
 
-$ clang++ power_law.cpp -o power_law `pkg-config --cflags --libs gammalib`
+$ clang++ power_law.cpp -o power_law \
+  `pkg-config --cflags --libs gammalib`
 $ ./power_law 
 2.06695e-11
 
