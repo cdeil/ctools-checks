3353a91b1a340a2301f4162e1f9898e42433e073
Christoph Deil <Deil.Christoph@gmail.com>
2014-02-24 10:26:37 +0100

    update

---
+++ energy_dispersion/make.sh
@@ -0,0 +1,19 @@
+MODEL=model.xml
+EVENTS=events.fits
+CALDB=$GAMMALIB/share/caldb/cta
+IRF=kb_I_50h_v3
+
+ctobssim infile=$MODEL outfile=$EVENTS \
+         caldb=$CALDB irf=$IRF \
+         ra=0 dec=0 rad=1 tmin=0 tmax=3600 \
+         emin=0.1 emax=100 edisp=no
+
+ctlike infile=$EVENTS srcmdl=$MODEL \
+       outmdl=results_edisp_no.xml logfile=results_edisp_no_ctlike.log \
+       caldb=$CALDB irf=$IRF
+       edisp=no
+
+ctlike infile=$EVENTS srcmdl=$MODEL \
+       outmdl=results_edisp_yes.xml logfile=results_edisp_yes_ctlike.log \
+       caldb=$CALDB irf=$IRF
+       edisp=yes
