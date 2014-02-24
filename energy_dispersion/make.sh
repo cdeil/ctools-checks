1cd8cf122ae963d0e887c7ba3429132fef542c95
Christoph Deil <Deil.Christoph@gmail.com>
2014-02-24 10:34:40 +0100

    update

--- energy_dispersion/make.sh
+++ energy_dispersion/make.sh
@@ -2,9 +2,10 @@ MODEL=model.xml
 EVENTS=events.fits
 CALDB=$GAMMALIB/share/caldb/cta
 IRF=kb_I_50h_v3
+SEED=42
 
 ctobssim infile=$MODEL outfile=$EVENTS \
-         caldb=$CALDB irf=$IRF \
+         caldb=$CALDB irf=$IRF seed=$SEED \
          ra=0 dec=0 rad=1 tmin=0 tmax=3600 \
          emin=0.1 emax=100 edisp=yes
 
